388c451fff6de0a56fee04cb683a09ce34beec0c
Preston Malen <90215402+pmalen327@users.noreply.github.com>
2022-03-06 23:45:44 -0700

    Update dynamic array.java

--- Dynamic Array/dynamic array.java
+++ Dynamic Array/dynamic array.java
@@ -2,10 +2,7 @@ package a6;
 
 /**
  * A DynamicArray behaves like an array of strings, except that it can grow and
- * shrink. It is indexed beginning with zero. When a DynamicArray is created, it
- * is empty. Methods are provided to report on the size, add, get, set, and
- * remove elements.
- * 
+ * shrink. It is indexed beginning with zero.
  * @author Preston Malen
  *
  */
